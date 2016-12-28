0a28907741843813dadaf31b7007d38970f70088
daniele <d.cariboni@pingpongstars.it>
2016-12-28 17:38:00 +0100

    pagine interne e menu

--- www/js/app.js
+++ www/js/app.js
@@ -2,3 +2,16 @@
 $("body").append("<p>questo è aggiunto da geiquiri!</p>");
 //$("body p").html("modifico il testo");
 //})
+
+$("#header .topmenu a").on("click",function(ev){
+	ev.preventDefault();
+
+	var dest = $(this).attr("data-href");
+	$("body").append("<p>mi hanno pigiato"+dest+"</p>");
+	if (window.location.href.indexOf(dest)>=0){
+		$("body").append("<p>me pias no:"+dest+"</p>");
+		return;
+	}
+	$("body").append("<p>mi dirigo verso"+dest+"</p>");
+	window.location.href=dest;
+})
